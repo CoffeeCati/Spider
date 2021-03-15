27287e214a0cf153053e26c822ece5f786fe31ad
刘硕 <liushuo@liushuodeMacBook-Air.local>
2021-03-15 15:53:52 +0800

    补充fork调用及修改文件语法

--- python retrospect/Process-fork.py
+++ python retrospect/Process-fork.py
@@ -8,11 +8,11 @@ pyhton3 实现多进程方式：
 import os
 if __name__ == '__main__':
     print('current Process (%s)  start ...' % (os.getpid()))   # os.getpid()获取当前进程ID
-pid = os.fork()     # fork调用一次返回两次
+pid = os.fork()     # fork调用一次返回两次 <!-- 相当于获取当前进程的子进程ID -->
 if pid < 0:
     print('Error in fork')
 elif pid == 0:
-    print("I'm child process(%s) and my parent process is (%s)", (os.getpid(), os.getppid()))
+    print("I'm child process(%s) and my parent process is (%s)" % (os.getpid(), os.getppid()))
 else:
     print('I(%s) created a child process (%s)' % (os.getpid(), pid))
 
