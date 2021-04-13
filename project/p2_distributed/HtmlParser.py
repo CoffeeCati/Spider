21cf0d780b18b6bd23b4d83f4fa779566e4527cb
刘硕 <liushuo@liushuodeMacBook-Air.local>
2021-04-13 16:33:00 +0800

    爬虫节点的解析器和下载器

---
+++ project/p2_distributed/HtmlParser.py
@@ -0,0 +1,37 @@
+import re
+from bs4 import BeautifulSoup
+from urllib import parse
+
+
+class HtmlParser():
+    def paser(self, page_url, html_cont):
+        '''
+        用于解析URL，抽取URL和数据
+        :param page_url: 下载页面的URL
+        :param html_cont: 下载的网页内容
+        :return: 返回URL和数据
+        '''
+        if page_url is None or html_cont is None:
+            return
+        soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
+        new_urls = self._get_new_urls(page_url, soup)
+        new_data = self._get_new_data(page_url, soup)
+        return new_urls, new_data
+
+    def _get_new_urls(self, page_url, soup):
+        new_urls = set()
+        links = soup.find_all('a', href=re.compile(r'/item/.+'))
+        for link in links:
+            new_url = link['href']
+            new_full_url = parse.urljoin(page_url, new_url)
+            new_urls.add(new_full_url)
+        return new_urls
+
+    def _get_new_data(self, page_url, soup):
+        data = {}
+        data['url'] = page_url
+        title = soup.find('dd', class_='lemmaWgt-lemmaTitle-title').find('h1')
+        data['title'] = title.get_text()
+        summary = soup.find('div', class_='lemma-summary')
+        data['summary'] = summary.get_text()
+        return data
